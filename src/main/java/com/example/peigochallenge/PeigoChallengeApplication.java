4a1a8d8675a6330dd9bd96e3769b7539710fe2a1
jhon <jhont285@gmail.com>
2022-10-26 07:34:22 -0500

    Spring initializr template

---
+++ src/main/java/com/example/peigochallenge/PeigoChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.peigochallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeigoChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeigoChallengeApplication.class, args);
+    }
+
+}
